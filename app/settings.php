8ee9fbe54e6e16a316f40e9a39186891012250fc
Rodrigo Tavares <rodrigost23@gmail.com>
2020-03-07 13:36:48 -0400

    Enable log to file on PHP

--- app/settings.php
+++ app/settings.php
@@ -13,7 +13,7 @@ return function (ContainerBuilder $containerBuilder) {
             'displayErrorDetails' => true, // Should be set to false in production
             'logger' => [
                 'name' => 'slim-app',
-                'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+                'path' => /* isset($_ENV['docker']) ? 'php://stdout' : */ __DIR__ . '/../logs/app.log',
                 'level' => Logger::DEBUG,
             ],
             'doctrine' => [
